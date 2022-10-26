fc82c70429f1372ac6f33b723ada89e58de9562c
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-26 15:36:51 +0200

    fine esercizio con bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,15 @@
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: "Hello World !",
+            url: "https://picsum.photos/id/1/500/300"
+        }
+    }
+}).mount("#app")
+
